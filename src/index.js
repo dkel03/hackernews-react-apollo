fc791e6a509df636ea5f1ad86530e9716c2b1ea4
Jay_Kim <dkel03@naver.com>
2020-06-14 22:50:40 +0900

    pagination
    Signed-off-by: Jay_Kim <dkel03@naver.com>

--- src/index.js
+++ src/index.js
@@ -1,27 +1,65 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/index.css";
-import App from "./components/App";
+import { BrowserRouter } from 'react-router-dom'
 import * as serviceWorker from "./serviceWorker";
 
 import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
+import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+import App from "./components/App";
+import { AUTH_TOKEN } from './constants'
+import "./styles/index.css";
 
 const httpLink = createHttpLink({
   uri: "https://hackernews-back.herokuapp.com/",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const wsLink = new WebSocketLink({
+  uri: `ws://hackernews-back.herokuapp.com`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
